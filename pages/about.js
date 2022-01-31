844a55c2b06e1a9628273409bd9f32e6a748da9a
Josiah Sanchez <josiah.rasanchez@gmail.com>
2022-01-30 17:57:55 -0700

    Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,39 @@
+import Head from "next/head";
+
+export const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keyword" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+          pellentesque ante neque, vel consectetur arcu commodo vitae. Integer
+          accumsan enim ut turpis placerat cursus. Nulla facilisi. Nullam et sem
+          volutpat, congue massa eu, laoreet massa. Ut eu commodo ligula. In hac
+          habitasse platea dictumst. Nullam ut vulputate ex. Ut sed faucibus
+          massa. Cras sed ante tellus. Pellentesque leo ante, lobortis et arcu
+          ut, mattis tempor ligula. Nam vel tortor tortor. Duis tellus libero,
+          lacinia in sollicitudin sit amet, fringilla eu nibh. Curabitur semper
+          volutpat malesuada. Phasellus bibendum ac dolor dignissim volutpat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+          pellentesque ante neque, vel consectetur arcu commodo vitae. Integer
+          accumsan enim ut turpis placerat cursus. Nulla facilisi. Nullam et sem
+          volutpat, congue massa eu, laoreet massa. Ut eu commodo ligula. In hac
+          habitasse platea dictumst. Nullam ut vulputate ex. Ut sed faucibus
+          massa. Cras sed ante tellus. Pellentesque leo ante, lobortis et arcu
+          ut, mattis tempor ligula. Nam vel tortor tortor. Duis tellus libero,
+          lacinia in sollicitudin sit amet, fringilla eu nibh. Curabitur semper
+          volutpat malesuada. Phasellus bibendum ac dolor dignissim volutpat.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,31 @@
+export const Test = () => {
+  return (
+    <div>
+      <h1>Test Ninja</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        pellentesque ante neque, vel consectetur arcu commodo vitae. Integer
+        accumsan enim ut turpis placerat cursus. Nulla facilisi. Nullam et sem
+        volutpat, congue massa eu, laoreet massa. Ut eu commodo ligula. In hac
+        habitasse platea dictumst. Nullam ut vulputate ex. Ut sed faucibus
+        massa. Cras sed ante tellus. Pellentesque leo ante, lobortis et arcu ut,
+        mattis tempor ligula. Nam vel tortor tortor. Duis tellus libero, lacinia
+        in sollicitudin sit amet, fringilla eu nibh. Curabitur semper volutpat
+        malesuada. Phasellus bibendum ac dolor dignissim volutpat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        pellentesque ante neque, vel consectetur arcu commodo vitae. Integer
+        accumsan enim ut turpis placerat cursus. Nulla facilisi. Nullam et sem
+        volutpat, congue massa eu, laoreet massa. Ut eu commodo ligula. In hac
+        habitasse platea dictumst. Nullam ut vulputate ex. Ut sed faucibus
+        massa. Cras sed ante tellus. Pellentesque leo ante, lobortis et arcu ut,
+        mattis tempor ligula. Nam vel tortor tortor. Duis tellus libero, lacinia
+        in sollicitudin sit amet, fringilla eu nibh. Curabitur semper volutpat
+        malesuada. Phasellus bibendum ac dolor dignissim volutpat.
+      </p>
+    </div>
+  );
+};
+
+export default Test;
